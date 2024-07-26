2b59386b9f3b668e7fe76da410af79d82be5bea7
Hendrik Nieländer <hendrik@nielaender.de>
2024-07-26 21:35:36 +0200

    chore: apply build path fo zig 0.13.0

--- build.zig
+++ build.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const Build = std.Build;
+
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
@@ -19,7 +21,7 @@ pub fn build(b: *std.Build) void {
         .name = "http2.zig",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "http2.zig" },
+        .root_source_file = b.path("http2.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -29,7 +31,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "http2.zig" },
+        .root_source_file = b.path("http2.zig"),
         .target = target,
         .optimize = optimize,
     });
