87bd14949a5811245a64e1e5c58d747c6fa80e1d
Hendrik Nieländer <hendrik@nielaender.de>
2024-08-14 22:37:40 +0200

    build: use correct path

--- build.zig
+++ build.zig
@@ -22,7 +22,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "http2.zig",
-        .root_source_file = b.path("http2.zig"),
+        .root_source_file = b.path("http2/connection.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -41,7 +41,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("http2.zig"),
+        .root_source_file = b.path("http2/connection.zig"),
         .target = target,
         .optimize = optimize,
     });
